92184cb84a2e22a1e1d50c27f0121195ccc9a8be
Henning Håkonsen <henninghaakonsen@me.com>
2020-01-17 14:52:21 +0100

    Rydder opp i merge conflict.

--- src/main/kotlin/no/nav/familie/oppdrag/rest/OppdragController.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/rest/OppdragController.kt
@@ -5,19 +5,12 @@ import no.nav.familie.kontrakter.felles.oppdrag.Utbetalingsoppdrag
 import no.nav.familie.oppdrag.domene.OppdragId
 import no.nav.familie.oppdrag.iverksetting.OppdragMapper
 import no.nav.familie.oppdrag.repository.OppdragProtokollStatus
-import no.nav.familie.oppdrag.iverksetting.OppdragMapper
 import no.nav.familie.oppdrag.service.OppdragService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.format.annotation.DateTimeFormat
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -53,6 +46,8 @@ class OppdragController(@Autowired val oppdragService: OppdragService,
                             ResponseEntity.ok(Ressurs.success(it))
                         }
                 )
+    }
+
     @PostMapping(path = ["/grensesnittavstemming/{fagsystem}"])
     fun sendGrensesnittavstemming(@PathVariable("fagsystem") fagsystem: String,
                        @RequestParam("fom") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) fom: LocalDateTime,
