bbddddb0f98421bc879e51c2b9866ec5c6d5fe63
Helga Nyrud <45488635+helganyrud@users.noreply.github.com>
2020-01-31 08:45:37 +0100

    Lagt til metrikker på grensesnittavstemming (#35)

--- src/main/kotlin/no/nav/familie/oppdrag/service/AvstemmingService.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/service/AvstemmingService.kt
@@ -1,8 +1,10 @@
 package no.nav.familie.oppdrag.service
 
+import io.micrometer.core.instrument.Metrics;
 import no.nav.familie.oppdrag.avstemming.AvstemmingSender
 import no.nav.familie.oppdrag.grensesnittavstemming.AvstemmingMapper
 import no.nav.familie.oppdrag.repository.OppdragLagerRepository
+import no.nav.virksomhet.tjenester.avstemming.meldinger.v1.Grunnlagsdata
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -18,6 +20,11 @@ class AvstemmingService(
         val avstemmingMapper = AvstemmingMapper(oppdragSomSkalAvstemmes, fagsystem)
         val meldinger = avstemmingMapper.lagAvstemmingsmeldinger()
 
+        if (meldinger.isEmpty()) {
+            LOG.info("Ingen oppdrag å gjennomføre grensesnittavstemming for.")
+            return
+        }
+
         LOG.info("Utfører grensesnittavstemming for id: ${avstemmingMapper.avstemmingId}, ${meldinger.size} antall meldinger.")
 
         meldinger.forEach {
@@ -25,10 +32,30 @@ class AvstemmingService(
         }
 
         LOG.info("Fullført grensesnittavstemming for id: ${avstemmingMapper.avstemmingId}")
+
+        oppdaterMetrikker(fagsystem, meldinger[1].grunnlag)
+    }
+
+    fun oppdaterMetrikker(fagsystem: String, grunnlag: Grunnlagsdata) {
+        Metrics.counter("familie.oppdrag.grensesnittavstemming","fagsystem", fagsystem, "status", Status.GODKJENT.status,
+                "beskrivelse", Status.GODKJENT.beskrivelse).increment(grunnlag.godkjentAntall.toDouble())
+        Metrics.counter("familie.oppdrag.grensesnittavstemming","fagsystem", fagsystem, "status", Status.AVVIST.status,
+                "beskrivelse", Status.AVVIST.beskrivelse).increment(grunnlag.avvistAntall.toDouble())
+        Metrics.counter("familie.oppdrag.grensesnittavstemming","fagsystem", fagsystem, "status", Status.MANGLER.status,
+                "beskrivelse", Status.MANGLER.beskrivelse).increment(grunnlag.manglerAntall.toDouble())
+        Metrics.counter("familie.oppdrag.grensesnittavstemming","fagsystem", fagsystem, "status", Status.VARSEL.status,
+                "beskrivelse", Status.VARSEL.beskrivelse).increment(grunnlag.varselAntall.toDouble())
     }
 
     companion object {
         val LOG = LoggerFactory.getLogger(AvstemmingService::class.java)
     }
 
+}
+
+enum class Status(val status : String, val beskrivelse : String) {
+    GODKJENT("godkjent", "Antall oppdrag som har fått OK kvittering (alvorlighetsgrad 00)."),
+    AVVIST("avvist", "Antall oppdrag som har fått kvittering med funksjonell eller teknisk feil, samt ukjent (alvorlighetsgrad 08 og 12)."),
+    MANGLER("mangler", "Antall oppdrag hvor kvittering mangler."),
+    VARSEL("varsel", "Antall oppdrag som har fått kvittering med mangler (alvorlighetsgrad 04).")
 }
