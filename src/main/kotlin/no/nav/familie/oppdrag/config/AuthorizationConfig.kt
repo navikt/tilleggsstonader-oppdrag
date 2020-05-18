ec835bbbd89a5f6ee5f61b1ec889431bf8170a89
Henning Håkonsen <henninghaakonsen@me.com>
2020-05-18 18:46:09 +0200

    Disabler auth filter for e2e og dev profilene.

--- src/main/kotlin/no/nav/familie/oppdrag/config/AuthorizationConfig.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/config/AuthorizationConfig.kt
@@ -1,19 +1,27 @@
-package no.nav.familie.oppdrag.config
+package no.nav.familie.ba.sak.config
 
 import no.nav.familie.sikkerhet.AuthorizationFilter
 import no.nav.familie.sikkerhet.OIDCUtil
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 
 @Configuration
 class AuthorizationConfig(
         private val oidcUtil: OIDCUtil,
         @Value("\${ACCEPTED_CLIENTS}")
-        private val acceptedClients: List<String>
+        private val acceptedClients: List<String>,
+        private val environment: Environment
 ) {
+
     @Bean
     fun authorizationFilter(): AuthorizationFilter {
-        return AuthorizationFilter(oidcUtil, acceptedClients)
+        return AuthorizationFilter(oidcUtil = oidcUtil,
+                                   acceptedClients = acceptedClients,
+                                   disabled = environment.activeProfiles.any {
+                                       listOf("e2e", "dev")
+                                               .contains(it.trim(' '))
+                                   })
     }
 }
