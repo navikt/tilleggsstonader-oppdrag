45500eaa213357d3f02d4fa5f987e0480808d012
Marcus Gustafson <marcus.gustafson@computas.com>
2020-10-06 12:57:53 +0200

    import optimalisering

--- src/main/kotlin/no/nav/familie/oppdrag/avstemming/AvstemmingSenderMock.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/avstemming/AvstemmingSenderMock.kt
@@ -1,16 +1,8 @@
 package no.nav.familie.oppdrag.avstemming
 
-import no.nav.familie.oppdrag.grensesnittavstemming.JaxbGrensesnittAvstemmingsdata
-import no.nav.familie.oppdrag.konsistensavstemming.JaxbKonsistensavstemming
 import no.nav.virksomhet.tjenester.avstemming.informasjon.konsistensavstemmingsdata.v1.Konsistensavstemmingsdata
-import no.nav.virksomhet.tjenester.avstemming.informasjon.konsistensavstemmingsdata.v1.SendAsynkronKonsistensavstemmingsdataRequest
 import no.nav.virksomhet.tjenester.avstemming.meldinger.v1.Avstemmingsdata
-import no.nav.virksomhet.tjenester.avstemming.v1.SendAsynkronKonsistensavstemmingsdata
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Profile
-import org.springframework.jms.JmsException
-import org.springframework.jms.core.JmsTemplate
 import org.springframework.stereotype.Service
 
 @Service
