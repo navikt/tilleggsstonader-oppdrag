5a53339e73c6b506bf53b5833bc8c1d1f0db09b7
Geir Roger Moen <geir.roger.moen@nav.no>
2020-12-18 11:11:38 +0100

    WIP FagOmrådeKode for barnetrygd gjennom infotrygd

--- src/main/kotlin/no/nav/familie/oppdrag/repository/SimuleringLagerTjenesteE2E.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/repository/SimuleringLagerTjenesteE2E.kt
@@ -3,14 +3,14 @@ package no.nav.familie.oppdrag.repository
 class SimuleringLagerTjenesteE2E : SimuleringLagerTjeneste {
 
     override fun lagreINyTransaksjon(simuleringsLager: SimuleringLager) {
-        TODO("Not yet implemented")
+        TODO("Not yet implemented") //NOSONAR
     }
 
     override fun oppdater(simuleringsLager: SimuleringLager) {
-        TODO("Not yet implemented")
+        TODO("Not yet implemented") //NOSONAR
     }
 
     override fun finnAlleSimuleringsLager(): List<SimuleringLager> {
-        TODO("Not yet implemented")
+        TODO("Not yet implemented") //NOSONAR
     }
 }
--- src/main/kotlin/no/nav/familie/oppdrag/simulering/repository/FagOmrådeKode.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/simulering/repository/FagOmrådeKode.kt
@@ -2,7 +2,7 @@ package no.nav.familie.oppdrag.simulering.repository
 
 enum class FagOmrådeKode(val kode: String) {
     BARNETRYGD("BA"),
-    BARNETRYGD_INFOTRYGD("BAIT"),
+    BARNETRYGD_INFOTRYGD("IT05"),
     ENSLIG_FORSØRGER("EF"),
     ENSLIG_FORSØRGER_REFUSJON("EFREF"),
     KONTANTSTØTTE("KS");
