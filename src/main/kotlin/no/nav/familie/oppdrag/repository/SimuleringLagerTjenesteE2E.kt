0446a09c07488fc99b7601009e852c540e9437b3
Saikat Halder <saikat.halder@nav.no>
2021-01-04 10:04:35 +0100

    WIP ekskludert SimuleringLagerTjenesteE2E fra sonar analysis

--- src/main/kotlin/no/nav/familie/oppdrag/repository/SimuleringLagerTjenesteE2E.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/repository/SimuleringLagerTjenesteE2E.kt
@@ -3,14 +3,14 @@ package no.nav.familie.oppdrag.repository
 class SimuleringLagerTjenesteE2E : SimuleringLagerTjeneste {
 
     override fun lagreINyTransaksjon(simuleringsLager: SimuleringLager) {
-        TODO("Not yet implemented") //NOSONAR
+        TODO("Not yet implemented")
     }
 
     override fun oppdater(simuleringsLager: SimuleringLager) {
-        TODO("Not yet implemented") //NOSONAR
+        TODO("Not yet implemented")
     }
 
     override fun finnAlleSimuleringsLager(): List<SimuleringLager> {
-        TODO("Not yet implemented") //NOSONAR
+        TODO("Not yet implemented")
     }
 }
