60854e14153d94311145856b9be3cf7835bb8433
Saikat Halder <saikat.halder@nav.no>
2021-01-11 16:16:15 +0100

    forbedret enhetstester

--- src/test/kotlin/no/nav/familie/oppdrag/simulering/mock/SimuleringGeneratorTest.kt
+++ src/test/kotlin/no/nav/familie/oppdrag/simulering/mock/SimuleringGeneratorTest.kt
@@ -1,130 +1,113 @@
 package no.nav.familie.oppdrag.simulering.mock
 
+import no.nav.system.os.entiteter.beregningskjema.BeregningStoppnivaaDetaljer
 import no.nav.system.os.entiteter.typer.simpletypes.KodeStatusLinje
 import no.nav.system.os.tjenester.simulerfpservice.simulerfpservicegrensesnitt.SimulerBeregningRequest
 import no.nav.system.os.tjenester.simulerfpservice.simulerfpserviceservicetypes.Oppdrag
 import no.nav.system.os.tjenester.simulerfpservice.simulerfpserviceservicetypes.Oppdragslinje
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.math.BigDecimal
 
 internal class SimuleringGeneratorTest {
+
     var simuleringGenerator = SimuleringGenerator()
+
     @Test
     fun SimuleringTestPositiv() {
-        val response = simuleringGenerator.opprettSimuleringsResultat(simTestDataBaNy())
-        Assertions.assertThat(response.response.simulering.gjelderId).isEqualTo("12345678901")
-        Assertions.assertThat(response.response.simulering.beregningsPeriode.size).isEqualTo(1)
-        Assertions.assertThat(response.response.simulering.beregningsPeriode[0].beregningStoppnivaa.size).isEqualTo(5)
+        val oppdragGjelderId = "12345678901"
+        val kodeEndring = "ENDR"
+        val request: SimulerBeregningRequest = opprettSimulerBeregningRequest(oppdragGjelderId, kodeEndring)
+        request.request.oppdrag.oppdragslinje.add(opprettOppdragslinje("NY", null, 2339, oppdragGjelderId))
+
+        val response = simuleringGenerator.opprettSimuleringsResultat(request)
+        assertThat(response.response.simulering.gjelderId).isEqualTo(oppdragGjelderId)
+        assertThat(response.response.simulering.beregningsPeriode.size).isEqualTo(1)
+        assertThat(response.response.simulering.beregningsPeriode[0].beregningStoppnivaa.size).isEqualTo(5)
+        var detaljer: List<BeregningStoppnivaaDetaljer> =
+                response.response.simulering.beregningsPeriode[0].beregningStoppnivaa[0].beregningStoppnivaaDetaljer;
+        detaljer.sortedBy { beregningStoppnivaaDetaljer -> beregningStoppnivaaDetaljer.behandlingskode }
+        assertThat(detaljer.size).isEqualTo(1) //finnes ikke feilutbetaling
     }
+
     @Test
     fun SimuleringTestReduksjon() {
-        val response = simuleringGenerator.opprettSimuleringsResultat(simTestDataBaReduksjon())
-        Assertions.assertThat(response.response.simulering.gjelderId).isEqualTo("12345678902")
-        Assertions.assertThat(response.response.simulering.beregningsPeriode[0].beregningStoppnivaa[0].beregningStoppnivaaDetaljer.size).isEqualTo(3)
-        Assertions.assertThat(response.response.simulering.beregningsPeriode[0].beregningStoppnivaa[0].beregningStoppnivaaDetaljer[1].typeKlasse).isEqualTo("FEIL")
-        Assertions.assertThat(response.response.simulering.beregningsPeriode[0].beregningStoppnivaa[0].beregningStoppnivaaDetaljer[1].belop).isEqualTo(BigDecimal.valueOf(130))
+        val oppdragGjelderId = "12345678902"
+        val kodeEndring = "ENDR"
+        val request: SimulerBeregningRequest = opprettSimulerBeregningRequest(oppdragGjelderId, kodeEndring)
+        request.request.oppdrag.oppdragslinje.add(opprettOppdragslinje(kodeEndring, KodeStatusLinje.OPPH, 1330, oppdragGjelderId))
+        request.request.oppdrag.oppdragslinje.add(opprettOppdragslinje("NY", null, 1200, oppdragGjelderId))
+
+        val response = simuleringGenerator.opprettSimuleringsResultat(request)
+        assertThat(response.response.simulering.gjelderId).isEqualTo(oppdragGjelderId)
+        assertThat(response.response.simulering.beregningsPeriode[0].beregningStoppnivaa.size).isEqualTo(5)
+
+        var detaljer: List<BeregningStoppnivaaDetaljer> =
+                response.response.simulering.beregningsPeriode[0].beregningStoppnivaa[0].beregningStoppnivaaDetaljer
+        assertThat(detaljer.size).isEqualTo(3) //finnes feilutbetaling, positiv og negatliv ytelse postering
+        detaljer.sortedBy { beregningStoppnivaaDetaljer -> beregningStoppnivaaDetaljer.behandlingskode }
+        assertThat(detaljer[0].typeKlasse).isEqualTo("YTEL")
+        assertThat(detaljer[0].belop).isEqualTo(BigDecimal.valueOf(1200))
+        assertThat(detaljer[1].typeKlasse).isEqualTo("FEIL")
+        assertThat(detaljer[1].belop).isEqualTo(BigDecimal.valueOf(130))
+        assertThat(detaljer[2].typeKlasse).isEqualTo("YTEL")
+        assertThat(detaljer[2].belop).isEqualTo(BigDecimal.valueOf(-1330))
     }
+
     @Test
     fun SimuleringTestOpphør() {
-        val response = simuleringGenerator.opprettSimuleringsResultat(simTestDataBaOpphør())
-        Assertions.assertThat(response.response.simulering.gjelderId).isEqualTo("12345678903")
-        Assertions.assertThat(response.response.simulering.beregningsPeriode[0].beregningStoppnivaa[0].beregningStoppnivaaDetaljer[1].typeKlasse).isEqualTo("FEIL")
-        Assertions.assertThat(response.response.simulering.beregningsPeriode[0].beregningStoppnivaa[0].beregningStoppnivaaDetaljer[1].belop).isEqualTo(BigDecimal.valueOf(1330))
+        val oppdragGjelderId = "12345678903"
+        val kodeEndring = "ENDR"
+        val request: SimulerBeregningRequest = opprettSimulerBeregningRequest(oppdragGjelderId, kodeEndring)
+        request.request.oppdrag.oppdragslinje.add(opprettOppdragslinje(kodeEndring, KodeStatusLinje.OPPH, 1330, oppdragGjelderId))
+
+        val response = simuleringGenerator.opprettSimuleringsResultat(request)
+        assertThat(response.response.simulering.gjelderId).isEqualTo(oppdragGjelderId)
+        assertThat(response.response.simulering.beregningsPeriode[0].beregningStoppnivaa.size).isEqualTo(5)
+
+        var detaljer: List<BeregningStoppnivaaDetaljer> =
+                response.response.simulering.beregningsPeriode[0].beregningStoppnivaa[0].beregningStoppnivaaDetaljer
+        assertThat(detaljer.size).isEqualTo(3) //finnes feilutbetaling, positiv og negatliv ytelse postering
+        detaljer.sortedBy { beregningStoppnivaaDetaljer -> beregningStoppnivaaDetaljer.behandlingskode }
+        assertThat(detaljer[0].typeKlasse).isEqualTo("YTEL")
+        assertThat(detaljer[0].belop).isEqualTo(BigDecimal.valueOf(1330))
+        assertThat(detaljer[1].typeKlasse).isEqualTo("FEIL")
+        assertThat(detaljer[1].belop).isEqualTo(BigDecimal.valueOf(1330))
+        assertThat(detaljer[2].typeKlasse).isEqualTo("YTEL")
+        assertThat(detaljer[2].belop).isEqualTo(BigDecimal.valueOf(-1330))
     }
 
-    private fun simTestDataBaNy(): SimulerBeregningRequest {
+    private fun opprettSimulerBeregningRequest(oppdragGjelderId: String, kodeEndring: String): SimulerBeregningRequest {
         val request = SimulerBeregningRequest()
         request.request =
-            no.nav.system.os.tjenester.simulerfpservice.simulerfpserviceservicetypes.SimulerBeregningRequest()
+                no.nav.system.os.tjenester.simulerfpservice.simulerfpserviceservicetypes.SimulerBeregningRequest()
         request.request.oppdrag = Oppdrag()
-        request.request.oppdrag.kodeEndring = "NY"
-        request.request.oppdrag.kodeFagomraade = "BA"
-        request.request.oppdrag.fagsystemId = "123456789"
-        request.request.oppdrag.oppdragGjelderId = "12345678901"
-        request.request.oppdrag.saksbehId = "saksbeh"
-        val oppdragslinje = Oppdragslinje()
-        oppdragslinje.kodeEndringLinje = "NY"
-        oppdragslinje.vedtakId = "2020-11-27"
-        oppdragslinje.delytelseId = "1122334455667700"
-        oppdragslinje.kodeKlassifik = "FPADATORD"
-        oppdragslinje.datoVedtakFom = "2020-07-27"
-        oppdragslinje.datoVedtakTom = "2020-11-08"
-        oppdragslinje.datoStatusFom = "2020-10-19"
-        oppdragslinje.sats = BigDecimal.valueOf(2339)
-        oppdragslinje.typeSats = "MND"
-        oppdragslinje.saksbehId = "saksbeh"
-        oppdragslinje.utbetalesTilId = "12345678901"
-        oppdragslinje.henvisning = "123456"
-        request.request.oppdrag.oppdragslinje.add(oppdragslinje)
-        return request
-    }
-
-    private fun simTestDataBaReduksjon(): SimulerBeregningRequest {
-        val request = SimulerBeregningRequest()
-        request.request =
-            no.nav.system.os.tjenester.simulerfpservice.simulerfpserviceservicetypes.SimulerBeregningRequest()
-        request.request.oppdrag = Oppdrag()
-        request.request.oppdrag.kodeEndring = "ENDR"
-        request.request.oppdrag.kodeFagomraade = "BA"
-        request.request.oppdrag.fagsystemId = "223456789"
-        request.request.oppdrag.oppdragGjelderId = "12345678902"
-        request.request.oppdrag.saksbehId = "saksbeh"
-        val oppdragslinje = Oppdragslinje()
-        oppdragslinje.kodeEndringLinje = "ENDR"
-        oppdragslinje.kodeStatusLinje = KodeStatusLinje.OPPH
-        oppdragslinje.vedtakId = "2020-11-27"
-        oppdragslinje.delytelseId = "1122334455667700"
-        oppdragslinje.kodeKlassifik = "FPADATORD"
-        oppdragslinje.datoVedtakFom = "2020-07-27"
-        oppdragslinje.datoVedtakTom = "2020-11-08"
-        oppdragslinje.datoStatusFom = "2020-07-27"
-        oppdragslinje.sats = BigDecimal.valueOf(1330)
-        oppdragslinje.typeSats = "MND"
-        oppdragslinje.saksbehId = "saksbeh"
-        oppdragslinje.utbetalesTilId = "12345678902"
-        oppdragslinje.henvisning = "123456"
-        request.request.oppdrag.oppdragslinje.add(oppdragslinje)
-        val oppdragslinje2 = Oppdragslinje()
-        oppdragslinje2.kodeEndringLinje = "NY"
-        oppdragslinje2.vedtakId = "2020-11-27"
-        oppdragslinje2.delytelseId = "1122334455667700"
-        oppdragslinje2.kodeKlassifik = "FPADATORD"
-        oppdragslinje2.datoVedtakFom = "2020-07-27"
-        oppdragslinje2.datoVedtakTom = "2020-11-08"
-        oppdragslinje2.sats = BigDecimal.valueOf(1200)
-        oppdragslinje2.typeSats = "MND"
-        oppdragslinje2.saksbehId = "saksbeh"
-        oppdragslinje2.utbetalesTilId = "12345678902"
-        oppdragslinje2.henvisning = "123456"
-        request.request.oppdrag.oppdragslinje.add(oppdragslinje2)
-        return request
-    }
-    private fun simTestDataBaOpphør(): SimulerBeregningRequest {
-        val request = SimulerBeregningRequest()
-        request.request =
-            no.nav.system.os.tjenester.simulerfpservice.simulerfpserviceservicetypes.SimulerBeregningRequest()
-        request.request.oppdrag = Oppdrag()
-        request.request.oppdrag.kodeEndring = "ENDR"
+        request.request.oppdrag.kodeEndring = kodeEndring
         request.request.oppdrag.kodeFagomraade = "BA"
         request.request.oppdrag.fagsystemId = "323456789"
-        request.request.oppdrag.oppdragGjelderId = "12345678903"
+        request.request.oppdrag.oppdragGjelderId = oppdragGjelderId
         request.request.oppdrag.saksbehId = "saksbeh"
+        return request
+    }
+
+    private fun opprettOppdragslinje(kodeEndringLinje: String,
+                                     kodeStatusLinje: KodeStatusLinje?,
+                                     sats: Long,
+                                     utbetalesTilId: String): Oppdragslinje {
         val oppdragslinje = Oppdragslinje()
-        oppdragslinje.kodeEndringLinje = "ENDR"
-        oppdragslinje.kodeStatusLinje = KodeStatusLinje.OPPH
+        oppdragslinje.kodeEndringLinje = kodeEndringLinje
+        oppdragslinje.kodeStatusLinje = kodeStatusLinje
         oppdragslinje.vedtakId = "2020-11-27"
         oppdragslinje.delytelseId = "1122334455667700"
         oppdragslinje.kodeKlassifik = "FPADATORD"
         oppdragslinje.datoVedtakFom = "2020-07-27"
         oppdragslinje.datoVedtakTom = "2020-11-08"
         oppdragslinje.datoStatusFom = "2020-07-27"
-        oppdragslinje.sats = BigDecimal.valueOf(1330)
+        oppdragslinje.sats = BigDecimal.valueOf(sats)
         oppdragslinje.typeSats = "MND"
         oppdragslinje.saksbehId = "saksbeh"
-        oppdragslinje.utbetalesTilId = "12345678903"
+        oppdragslinje.utbetalesTilId = utbetalesTilId
         oppdragslinje.henvisning = "123456"
-        request.request.oppdrag.oppdragslinje.add(oppdragslinje)
-        return request
+        return oppdragslinje
     }
 }
