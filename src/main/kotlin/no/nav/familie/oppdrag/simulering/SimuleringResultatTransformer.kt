030a51e268aa0d6103d7c5062d33906cd17e8ab5
Joakim Schweizer <joakim.schweizer@nav.no>
2021-01-19 16:10:25 +0100

    Notat om trailing spaces

--- src/main/kotlin/no/nav/familie/oppdrag/simulering/SimuleringResultatTransformer.kt
+++ src/main/kotlin/no/nav/familie/oppdrag/simulering/SimuleringResultatTransformer.kt
@@ -42,7 +42,7 @@ class SimuleringResultatTransformer {
         return SimulertPostering(
                 betalingType = utledBetalingType(detaljer.belop),
                 beløp = detaljer.belop,
-                fagOmrådeKode = FagOmrådeKode.fraKode(stoppnivaa.kodeFagomraade.trim()),
+                fagOmrådeKode = FagOmrådeKode.fraKode(stoppnivaa.kodeFagomraade.trim()), //Todo: fjerne.trim() når TØB har rettet trailing spaces-feilen (jira: TOB-1509)
                 fom = parseDato(detaljer.faktiskFom),
                 tom = parseDato(detaljer.faktiskTom),
                 forfallsdato = parseDato(stoppnivaa.forfall),
