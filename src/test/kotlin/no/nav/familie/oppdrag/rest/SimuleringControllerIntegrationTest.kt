9e59d15721308f0f6d8a85c77a61fb8ad81ae1fd
Marcus Gustafson <marcus.gustafson@computas.com>
2021-02-05 09:16:21 +0100

    Fikset import

--- src/test/kotlin/no/nav/familie/oppdrag/rest/SimuleringControllerIntegrationTest.kt
+++ src/test/kotlin/no/nav/familie/oppdrag/rest/SimuleringControllerIntegrationTest.kt
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
-import kotlin.test.Ignore
 import kotlin.test.assertEquals
 
 
