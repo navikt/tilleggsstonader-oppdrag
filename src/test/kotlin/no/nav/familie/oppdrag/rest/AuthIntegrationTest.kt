12a2ce0c500739a2e9a897661d295ae283f98883
Johan Blomgren <blommish@users.noreply.github.com>
2021-02-19 18:29:55 +0100

    Dette testoppsettet...

--- src/test/kotlin/no/nav/familie/oppdrag/rest/AuthIntegrationTest.kt
+++ src/test/kotlin/no/nav/familie/oppdrag/rest/AuthIntegrationTest.kt
@@ -1,18 +1,24 @@
 package no.nav.familie.oppdrag.rest
 
+import no.nav.familie.oppdrag.util.Containers
 import no.nav.familie.oppdrag.util.TestConfig
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.exchange
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.*
+import org.springframework.jms.annotation.EnableJms
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
 
 @RestController
@@ -28,7 +34,16 @@ class AuthController {
 @SpringBootTest(classes = [TestConfig::class],
                 properties = ["spring.cloud.vault.enabled=false"],
                 webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(initializers = [Containers.PostgresSQLInitializer::class, Containers.MQInitializer::class])
+@EnableJms
+@DisabledIfEnvironmentVariable(named = "CIRCLECI", matches = "true")
+@Testcontainers
 class AuthIntegrationTest {
+
+    companion object {
+        @Container var postgreSQLContainer = Containers.postgreSQLContainer
+        @Container var ibmMQContainer = Containers.ibmMQContainer
+    }
 
     @LocalServerPort
     private var port: Int = 0
